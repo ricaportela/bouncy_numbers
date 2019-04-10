cf92930a969154a76629a8e11a91a9c7bbac81e8
ricaportela <ricaportela@gmail.com>
2019-04-10 04:41:26 -0300

    Create Test Frequency

---
+++ bouncy_numbers.py
@@ -0,0 +1,40 @@
+from itertools import count
+"""This is the problem:
+
+Working from left-to-right if no digit is exceeded by the digit to its left it is called an increasing number; for example, 134468.
+Similarly if no digit is exceeded by the digit to its right it is called a decreasing number; for example, 66420.
+We shall call a positive integer that is neither increasing nor decreasing a "bouncy" number; for example, 155349.
+
+Clearly there cannot be any bouncy numbers below one-hundred, but just over half of the numbers below one-thousand (525) are bouncy. 
+In fact, the least number for which the proportion of bouncy numbers first reaches 50% is 538.
+Surprisingly, bouncy numbers become more and more common and by the time we reach 21780 the proportion of bouncy numbers is equal to 90%.
+
+Find the least number for which the proportion of bouncy numbers is exactly 99%.
+
+"""
+def number_is_increase(number: int) -> bool:
+    num_str = str(number)
+    previous = 0
+    for c in num_str:
+        c = int(c)
+        if previous > c:
+            return False
+        previous = c
+    return True
+
+def number_is_decrease(number: int) -> bool:
+    num_str = ''.join(reversed(str(number)))
+    return number_is_increase(int(num_str))
+
+def number_is_bouncy(number: int) -> bool:
+    return (not number_is_increase(number)) and (not number_is_decrease(number))
+
+def bouncy_number(freq: float ) -> int:
+    bouncy_qty = 0
+    for i in count(100):
+        if number_is_bouncy(i):
+            bouncy_qty += 1
+            freq_calculated = bouncy_qty / 1
+            if freq_calculated >= freq:
+                return i
+
---
+++ test/test_bouncy.py
@@ -0,0 +1,31 @@
+from bouncy_numbers import number_is_increase, number_is_decrease, number_is_bouncy
+
+
+def test_check_number_is_increase():
+    assert number_is_increase(134468)
+    assert number_is_increase(12)
+    assert number_is_increase(123)
+    assert number_is_increase(1235)
+
+def test_check_number_not_is_increase():
+    assert not number_is_increase(21)
+
+def test_check_number_is_decrease():
+    assert number_is_decrease(66420)
+    assert number_is_decrease(21)
+    assert number_is_decrease(321)
+
+def test_check_number_not_is_decrease():
+    assert not number_is_decrease(12)
+    assert not number_is_decrease(1231)
+    assert not number_is_decrease(34534)
+
+def test_check_number_is_bouncy():
+    assert number_is_bouncy(155349)
+
+
+"""In fact, the least number for which the proportion of bouncy numbers first reaches 50% is 538.
+   Find the least number for which the proportion of bouncy numbers is exactly 99%.
+"""
+def test_frequency():
+    assert number_is_bouncy(0.5) == 538
